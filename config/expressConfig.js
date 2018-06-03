3a4aa48ee51b911fe1c6f9b4066203e69d39641b
Robson Araujo Carmo <robsonaraujocarmo@hotmail.com>
2018-06-03 10:52:30 -0300

    liberado cors

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -17,7 +17,12 @@ module.exports = function () {
   app.use(bodyParser.json());
   app.use(auth.initialize());
   app.use('/api/v1', routes);
-  app.use(cors());
+  //app.use(cors());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 

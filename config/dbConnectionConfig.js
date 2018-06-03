2b876bf4cbdad31d4bd3d89d0ac4fe546cb0e554
Robson Araujo Carmo <robsonaraujocarmo@hotmail.com>
2018-06-03 08:35:47 -0300

    change database name

--- config/dbConnectionConfig.js
+++ config/dbConnectionConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DB_NAME: process.env.DB_NAME || 'hackUberUbike',
+    DB_NAME: process.env.DB_NAME || 'hackuberubike',
     DB_HOST: process.env.DB_HOST || 'ubike.mysql.database.azure.com',
     DB_USER: process.env.DB_USER || 'ubike@ubike',
     DB_PASSWORD: process.env.DB_PASSWORD || 'Uberbike123'

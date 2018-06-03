f956e67c3e562044f8e5c675d3a064191a3d5262
Robson Araujo Carmo <robsonaraujocarmo@hotmail.com>
2018-06-03 07:54:04 -0300

    change database config

--- config/dbConnectionConfig.js
+++ config/dbConnectionConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     DB_NAME: process.env.DB_NAME || 'hackUberUbike',
-    DB_HOST: process.env.DB_HOST || '127.0.0.1',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || ''
+    DB_HOST: process.env.DB_HOST || 'ubike.mysql.database.azure.com',
+    DB_USER: process.env.DB_USER || 'ubike@ubike',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'Uberbike123'
 }

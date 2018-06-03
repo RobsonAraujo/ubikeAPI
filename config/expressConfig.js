b8d13dbe0ec121709e6f8c5cc87ac5d2aa972b0a
Robson Araujo Carmo <robsonaraujocarmo@hotmail.com>
2018-06-03 10:56:35 -0300

    set headers

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -19,8 +19,17 @@ module.exports = function () {
   app.use('/api/v1', routes);
   //app.use(cors());
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
 
